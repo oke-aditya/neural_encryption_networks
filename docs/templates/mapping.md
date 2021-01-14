867bd6bdd4c8b7af4c70a0ac603b0d7e99768cd7
Aditya Oke <okeaditya315@gmail.com>
2021-01-14 17:49:54 +0530

    adds random mapping code

--- docs/templates/mapping.md
+++ docs/templates/mapping.md
@@ -2,5 +2,24 @@
 
 In order to one hot vectorize the data we need to use mapping.
 
-The hash maps generated have been done using the following algorithm
+The hash maps generated have been done using the following algorithm.
 
+```
+1. Store all the possible writable characters in a list
+characters = [chr(i) for i in range(32, 123)]
+
+2. Generate a random integer between a big range to be assigned
+
+3. For 100 iterations:
+    Picking out a random integer from a large range(around 2 power 55)
+    Appending the binary representation of the random integer
+    End for
+
+4. Shuffle all the sampled numbers that were picked.
+
+5. Assign individual mapping of the bits to the corresponding alphabets in a
+random order
+
+```
+
+```
--- neural_encryption_networks/src/utils.py
+++ neural_encryption_networks/src/utils.py
@@ -1,7 +1,44 @@
 import numpy as np
+import random
 from sklearn.metrics import accuracy_score
 
-__all__ = ["create_labels", "create_input_array", "change_output", "accuracy"]
+__all__ = ["create_labels", "create_input_array", "change_output",
+           "accuracy", "read_from_file", "sequence",
+           "random_paragraph_generator"]
+
+
+def read_from_file(file):
+    m = ""
+    with open(file, "r") as f:
+        m += f.readline()
+        m += " "
+    m.rstrip(" ")
+    return m
+
+
+def sequence(word_length):
+    characters = [chr(i) for i in range(32, 123)]
+    random.shuffle(characters)
+    word = "".join(characters[:word_length])
+    return word
+
+
+def random_paragraph_generator():
+    no_of_words = random.randint(1000, 1500)
+    para = ""
+    for i in range(no_of_words):
+        word_length = random.randint(1, 10)
+        word = sequence(word_length)
+        para += word
+        para += " "
+    return para.encode("utf-8")
+
+
+def convert(paragraph, hashmap):
+    output = []
+    for i in paragraph:
+        output.append(list(map(int, str(hashmap[i]))))
+    return output
 
 
 def create_labels(paragraph, hashmap):
