9e4e1d417c8a97e143b9fd012c695b2ea39678c4
Aditya Oke <okeaditya315@gmail.com>
2021-01-14 23:00:12 +0530

    more cleanup

--- neural_encryption_networks/src/utils.py
+++ neural_encryption_networks/src/utils.py
@@ -12,7 +12,27 @@ __all__ = [
     "read_from_file",
     "sequence",
     "random_paragraph_generator",
+    "generate_hashmap"
 ]
+
+
+def generate_hashmap(limit_min, limit_max):
+    characters = [chr(i) for i in range(32, 123)]
+    # characters.append(' ')
+    hash_map_values = []
+
+    for i in range(100):
+        a = random.randint(limit_min, limit_max)
+        hash_map_values.append(int(bin(a)[2:]))
+
+    # print(hash_map_values)
+
+    random.shuffle(characters)
+    random.shuffle(hash_map_values)
+    hash_map = dict(zip(characters, hash_map_values))
+    # print(hash_map)
+
+    return hash_map
 
 
 def read_from_file(file):
